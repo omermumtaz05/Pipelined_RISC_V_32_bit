8f9bc10ce61b0f978e47d0075bcb55c11b47156a
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-08 16:59:00 +0400

    Update mem_to_reg_mux.v

--- WB_stage/mem_to_reg_mux.v
+++ WB_stage/mem_to_reg_mux.v
@@ -1,9 +1,13 @@
 module mem_to_reg_mux(
+    input [31:0] mem_read_data,
+    input [31:0] ALU_result,
+    input mem_to_reg,
 
-
+    output [31:0] write_data
 
 );
 
-
+    assign write_data = ~mem_to_reg ? ALU_result : mem_read_data;
+       
 
 endmodule
