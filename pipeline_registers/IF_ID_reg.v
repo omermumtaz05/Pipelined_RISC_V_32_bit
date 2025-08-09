535afab74d36f24410e5aaac4c0ca37f33581c58
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-09 16:57:04 +0400

    Update IF_ID_reg.v
    verified

--- pipeline_registers/IF_ID_reg.v
+++ pipeline_registers/IF_ID_reg.v
@@ -1,3 +1,4 @@
+// Code your design here
 module IF_ID(
     input clock,
     input reset,
@@ -5,24 +6,25 @@ module IF_ID(
     input [31:0] input_pc_address,
     input [31:0] input_instruc,
 
-    output [31:0] out_pc_address,
-    output [31:0] output_instruc
+    output reg [31:0] out_pc_address,
+    output reg [31:0] output_instruc
 );
 
     always @ (posedge clock)
-        if(reset){
+      if(reset)
             begin
                 out_pc_address <= 32'b0;
-                output_instruc <= 32'b0
+                output_instruc <= 32'b0;
             end
-        }
-        else{
+      
+        
+        else
             begin
                 out_pc_address <= input_pc_address;
                 output_instruc <= input_instruc;
 
             end
-        }
+        
 
 endmodule
 
