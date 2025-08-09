a87b5c7d0c9637a64380dffd795fbe4ff36e3a2c
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-09 16:35:05 +0400

    Add files via upload

---
+++ EX_stage/ALUControl.v
@@ -0,0 +1,34 @@
+module ALUControl(
+    
+input funct7_bit_6,
+input [2:0] funct3,
+input [1:0] ALUOp,
+input reset, 
+
+output reg [3:0] control
+
+);
+  
+    always @ (*)
+        begin
+            if(reset)
+                control = 0;
+
+            else if(ALUOp == 2'b00)
+                control = 4'b0010;
+            else if(ALUOp == 2'b01)
+                control = 4'b0110;
+            else if(ALUOp == 2'b10)
+                begin
+                  if(funct7_bit_6 == 1'b0 && funct3 == 3'b0)
+                        control = 4'b0010; // add
+                  else if(funct7_bit_6 == 1'b1 && funct3 == 3'b0)
+                        control = 4'b0110; // sub
+                  else if(funct7_bit_6 == 1'b0 && funct3 == 3'b111)
+                        control = 4'b0000; // AND
+                    else if(funct7_bit_6 == 1'b0 && funct3 == 3'b110)
+                        control = 4'b0001; // OR;
+                end
+        end
+
+endmodule
