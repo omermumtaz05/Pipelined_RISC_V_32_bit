8c5b1a0661eae5238a4eaf54447d9a6fe6753167
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-13 15:39:04 +0400

    Update cpu_pkg.sv

--- pipeline_registers/cpu_pkg.sv
+++ pipeline_registers/cpu_pkg.sv
@@ -1,6 +1,13 @@
 package cpu_pkg;
 
 typedef struct packed {
+    // Data to be stored
+    reg [31:0] pc_address;
+    reg [31:0] instruc;
+    
+} if_id_data_t;
+
+typedef struct packed {
     // WB stage control
     reg WB_reg_write;
     reg WB_mem_to_reg;
@@ -13,7 +20,8 @@ typedef struct packed {
     // EX stage control signals
     reg [1:0] EX_ALU_Op;
     reg EX_ALU_Src;
-} pipeline_control_t_id_ex;
+
+} id_ex_control_t;
 
 typedef struct packed {
     // Data to be stored
@@ -24,6 +32,8 @@ typedef struct packed {
     reg [31:0] imm;
     reg [3:0] funct_inst_bits;
     reg [4:0] rd;
-} pipeline_data_t_id_ex;
+
+} id_ex_data_t;
+
 
 endpackage
