52c7eccb256aa32bf4e9163fc791da573e195fef
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-13 15:21:12 +0400

    Update cpu_pkg.sv
    changed logic to reg for modelsim

--- pipeline_registers/cpu_pkg.sv
+++ pipeline_registers/cpu_pkg.sv
@@ -1,39 +1,29 @@
 package cpu_pkg;
 
+typedef struct packed {
+    // WB stage control
+    reg WB_reg_write;
+    reg WB_mem_to_reg;
 
-typedef struct packed{
-    //WB stage control
-    logic WB_reg_write,
-    logic WB_mem_to_reg,
-
-    //Mem stage contorl signals
-    logic M_branch,
-    logic M_mem_read,
-    logic M_mem_write,
+    // Mem stage control signals
+    reg M_branch;
+    reg M_mem_read;
+    reg M_mem_write;
 
     // EX stage control signals
-    logic [1:0] EX_ALU_Op,
-    logic EX_ALU_Src,
+    reg [1:0] EX_ALU_Op;
+    reg EX_ALU_Src;
+} pipeline_control_t_id_ex;
 
-} pipeline_control_t_if_id;
+typedef struct packed {
+    // Data to be stored
+    reg [31:0] pc_address;
+    reg [31:0] reg_read_data1;
+    reg [31:0] reg_read_data2;
 
-typedef struct packed{
-     // data to be stored
-    logic [31:0] pc_address,
-    logic [31:0] reg_read_data1,
-    logic [31:0] reg_read_data2,
+    reg [31:0] imm;
+    reg [3:0] funct_inst_bits;
+    reg [4:0] rd;
+} pipeline_data_t_id_ex;
 
-    logic [31:0] imm,
-    logic [3:0] funct_inst_bits,
-    logic [4:0] rd,
-
-} pipeline_data_t_if_id;
-
-
-
-
-
-
-    
-endpackage cpu_pkg;
-
+endpackage
