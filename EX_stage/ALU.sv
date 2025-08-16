1272e45f958ed5adf27a0b1a1e15266d112d6eac
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-16 15:51:11 +0400

    Update and rename ALU.v to ALU.sv

--- EX_stage/ALU.sv
+++ EX_stage/ALU.sv
@@ -1,19 +1,19 @@
 module ALU(
-    input [31:0] A,
-    input [31:0] B,
-    input [3:0] control,
-    output zero, 
-    output reg [31:0] result
+    input logic [31:0] A,
+    input logic [31:0] B,
+    input logic [3:0] control,
+    output logic zero, 
+    output logic [31:0] result
 );
 
-    always @ (*)
+    always_comb @ (*)
         begin
             case(control)
                 4'b0000: result = A & B;
                 4'b0001: result = A | B;
                 4'b0010: result = A + B;
                 4'b0110: result = A - B;
-                default: result = 32'b0;
+                default: result = '0;
             endcase
         end
 
