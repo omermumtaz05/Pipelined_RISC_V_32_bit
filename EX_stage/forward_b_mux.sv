cadca2e4fedacf5235a467c43b44fedd781387d4
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-25 18:48:55 +0400

    Update forward_b_mux.sv

--- EX_stage/forward_b_mux.sv
+++ EX_stage/forward_b_mux.sv
@@ -1,18 +1,17 @@
 module forward_b_mux(
-    input logic [4:0] id_ex_rs2,
-    input logic [4:0] ex_mem_rd,
-    input logic [4:0] mem_wb_rd,
+    input logic [31:0] read_data_2,
+    input logic [31:0] ex_mem_out,
+    input logic [31:0] mem_wb_out,
 
     input logic [1:0] forward_b,
 
-    output logic [4:0] alu_inp_2
+    output logic [31:0] alu_inp_2
 );
 
-
-assign alu_inp_2 = (forward_b == 2'b00) ? id_ex_rs2:
-                       (forward_b == 2'b01) ? mem_wb_rd:
-                       (forward_b == 2'b10) ? ex_mem_rd:
-                       id_ex_rs2;
+    assign alu_inp_2 = (forward_b == 2'b00) ? read_data_2:
+                       (forward_b == 2'b01) ? mem_wb_out:
+                       (forward_b == 2'b10) ? ex_mem_out:
+                       read_data_2;
 
 
 
