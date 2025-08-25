44bca5ae1398903db950fffbfef7de583e90a750
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-25 18:46:08 +0400

    Update forward_a_mux.sv

--- EX_stage/forward_a_mux.sv
+++ EX_stage/forward_a_mux.sv
@@ -1,17 +1,17 @@
 module forward_a_mux(
-    input logic [4:0] id_ex_rs1,
-    input logic [4:0] ex_mem_rd,
-    input logic [4:0] mem_wb_rd,
+    input logic [31:0] read_data_1,
+    input logic [31:0] ex_mem_out,
+    input logic [31:0] mem_wb_out,
 
     input logic [1:0] forward_a,
 
-    output logic [4:0] alu_inp_1
+    output logic [31:0] alu_inp_1
 );
 
-    assign alu_inp_1 = (forward_a == 2'b00) ? id_ex_rs1:
-                       (forward_a == 2'b01) ? mem_wb_rd:
-                       (forward_a == 2'b10) ? ex_mem_rd:
-                       id_ex_rs1;
+    assign alu_inp_1 = (forward_a == 2'b00) ? read_data_1:
+                       (forward_a == 2'b01) ? mem_wb_out:
+                       (forward_a == 2'b10) ? ex_mem_out:
+                       read_data_1;
 
 
 
