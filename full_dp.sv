816d55468b6ccde38c9df36f296cdc62ab7187ee
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-27 12:22:52 +0400

    Update full_dp.sv

--- full_dp.sv
+++ full_dp.sv
@@ -11,7 +11,6 @@ module ProgramCounter(
   output logic [31:0] pc_out);
 
 
-
   always_ff @ (posedge clk)
     if(reset)
       pc_out <= '0;
@@ -57,67 +56,13 @@ module instruction_memory(
     logic [7:0] instr [127:0];
 
    	initial begin
-	// addi x3, x0, 20
-	instr[0] = 8'h93;
-	instr[1] = 8'h01;
+
+	// lw x2, 20(x0)
+	instr[0] = 8'h03;
+	instr[1] = 8'h21;
 	instr[2] = 8'h40;
 	instr[3] = 8'h01;
-	    
-	// lw x8, 120(x3)
-
-	instr[4] = 8'h03;
-	instr[5] = 8'hA4;
-	instr[6] = 8'h81;
-	instr[7] = 8'h07;
-
-	// add x10, x3, x8
-	instr[8] = 8'h33;
-	instr[9] = 8'h05;
-	instr[10] = 8'h34;
-	instr[11] = 8'h00;
-
-	// sub x11, x10, x8
-	instr[12] = 8'hb3;
-	instr[13] = 8'h05;
-	instr[14] = 8'h85;
-	instr[15] = 8'h40;
-
-	//beq x3, x11, 8
-	instr[16] = 8'h63;
-	instr[17] = 8'h84;
-	instr[18] = 8'hb1;
-	instr[19] = 8'h00;
-	    
-	// garbage address
-	instr[20] = 8'hxx;
-	instr[21] = 8'hxx;
-	instr[22] = 8'hxx;
-	instr[23] = 8'hxx;
-	    
-	// garbage address
-	instr[24] = 8'hxx;
-	instr[25] = 8'hxx;
-	instr[26] = 8'hxx;
-	instr[27] = 8'hxx;
-	
-	// and x13, x8, x3
-	instr[28] = 8'hb3;
-	instr[29] = 8'h76;
-	instr[30] = 8'h34;
-	instr[31] = 8'h00;
-	    
-	// or x14, x8, x3
-	instr[32] = 8'h33;
-	instr[33] = 8'h67;
-	instr[34] = 8'h34;
-	instr[35] = 8'h00;
-	    
-	// sw x3, 150(x0)
-	instr[36] = 8'h23;
-	instr[37] = 8'h2b;
-	instr[38] = 8'h30;
-	instr[39] = 8'h08;
-	    
+	   
     end
   
     always_ff @ (posedge clock)
@@ -260,8 +205,7 @@ endmodule
 
 module data_memory(
     input logic clk,
-    input logic  reset,
-    
+    input logic reset,
     input logic [31:0] address,
     input logic [31:0] writeData,
     input logic memRead,
@@ -271,26 +215,21 @@ module data_memory(
 );
 
     logic [7:0] data [127:0];
-
-    integer i;
+ 
+   
     always_ff @ (posedge clk)
-    begin
-       if(reset)
-       begin
-         	
-         	for(i = 0; i < 127; i = i + 1)
-                	data[i] <= '0;
-
-       end
-
-       else if(memWrite)
+   begin
+    if(reset)
+	data[20] = 8'h6D;
+    
+    else if(memWrite)
        begin
             data[address] <= writeData[7:0];
             data[address + 1] <= writeData[15:8];
             data[address + 2] <= writeData[23:16];
             data[address + 3] <= writeData[31:24];
        end
-    end
+     end
 
     always_comb
         if(memRead)
