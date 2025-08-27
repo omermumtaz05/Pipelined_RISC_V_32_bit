e7d76dc406157a94b981e7395183a7da775f050b
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-27 18:57:30 +0400

    Update full_dp.sv
    added fwd a and b muxes

--- full_dp.sv
+++ full_dp.sv
@@ -180,6 +180,41 @@ module ALU(
 
 endmodule
 
+module forward_a_mux(
+    input logic [31:0] read_data_1,
+    input logic [31:0] ex_mem_out,
+    input logic [31:0] mem_wb_out,
+
+    input logic [1:0] forward_a,
+
+    output logic [31:0] alu_inp_1
+);
+
+    assign alu_inp_1 = (forward_a == 2'b00) ? read_data_1:
+                       (forward_a == 2'b01) ? mem_wb_out:
+                       (forward_a == 2'b10) ? ex_mem_out:
+                       read_data_1;
+
+
+endmodule
+
+module forward_b_mux(
+    input logic [31:0] read_data_2,
+    input logic [31:0] ex_mem_out,
+    input logic [31:0] mem_wb_out,
+
+    input logic [1:0] forward_b,
+
+    output logic [31:0] alu_inp_2
+);
+
+    assign alu_inp_2 = (forward_b == 2'b00) ? read_data_2:
+                       (forward_b == 2'b01) ? mem_wb_out:
+                       (forward_b == 2'b10) ? ex_mem_out:
+                       read_data_2;
+
+endmodule
+
 module branch_pc_adder(
     input logic [31:0] PC_address,
     input logic [31:0] imm,
