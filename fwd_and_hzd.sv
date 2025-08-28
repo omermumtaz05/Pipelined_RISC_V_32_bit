cbd6bb99c98ff742c7022098e550aaa877d520cf
omermumtaz05 <omermumtaz05@gmail.com>
2025-08-28 22:59:20 +0400

    Create fwd_and_hzd.sv

---
+++ fwd_and_hzd.sv
@@ -0,0 +1,100 @@
+module forwarding_unit(
+    input logic [4:0] id_ex_rs1,
+    input logic [4:0] id_ex_rs2,
+    input logic [4:0] ex_mem_rd,
+    input logic [4:0] mem_wb_rd,
+
+    input logic ex_mem_reg_write,
+    input logic mem_wb_reg_write,
+
+    
+    output logic [1:0] forward_a,
+    output logic [1:0] forward_b
+);
+
+
+always_comb 
+begin
+    begin
+        if((ex_mem_rd != 0) && ex_mem_reg_write && (ex_mem_rd == id_ex_rs1))
+
+            forward_a = 2'b10;
+
+        else if((mem_wb_rd != 0) && mem_wb_reg_write 
+        && (mem_wb_rd == id_ex_rs1)
+        && !(ex_mem_reg_write && (ex_mem_rd != 0) // make sure prev instruction in ex/mem stage doesnt write to/read from same address
+        && ex_mem_rd == id_ex_rs1))
+
+            forward_a = 2'b01;
+
+        else
+            forward_a = '0;
+    end
+
+    begin
+        if((ex_mem_rd != 0) && ex_mem_reg_write && (ex_mem_rd == id_ex_rs2))
+
+            forward_b = 2'b10;
+
+        else if((mem_wb_rd != 0) && mem_wb_reg_write 
+        && (mem_wb_rd == id_ex_rs2)
+        && !(ex_mem_reg_write && (ex_mem_rd != 0) // make sure prev instruction in ex/mem stage doesnt write to/read from same address
+        && ex_mem_rd == id_ex_rs2))
+
+            forward_b = 2'b01;
+
+        else
+            forward_b = '0;
+    end
+
+end
+
+
+
+endmodule
+
+module hazard_detection(
+    input logic id_ex_mem_read,
+    input logic [4:0] id_ex_rd,
+    input logic [4:0] if_id_rs1,
+    input logic [4:0] if_id_rs2,
+
+
+    output logic PCWrite,
+    output logic if_id_write,
+    output logic control_mux_sig
+);
+
+
+always_comb
+begin
+
+    if(id_ex_mem_read &&
+      ((id_ex_rd == if_id_rs1) || 
+       (id_ex_rd ==  if_id_rs2)))
+
+        begin
+
+            PCWrite = 0;
+            if_id_write = 0;
+            control_mux_sig = 1; // input 0 into all control signals
+
+        end
+
+    else 
+    // regular instruction flow
+        begin
+
+            PCWrite = 1;
+            if_id_write = 1;
+            control_mux_sig = 0; //
+            
+        end
+
+
+
+end
+
+
+
+endmodule
