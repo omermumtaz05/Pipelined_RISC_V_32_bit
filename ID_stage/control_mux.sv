527251c7697ee069d0f9efab605e567434ddc476
omermumtaz05 <omermumtaz05@gmail.com>
2026-01-08 17:25:33 +0400

    Update control_mux.sv

--- ID_stage/control_mux.sv
+++ ID_stage/control_mux.sv
@@ -5,11 +5,11 @@ module control_mux(
 
     input logic stall,
 
-    output id_ex_control_t all_control_out
+    output id_ex_control_t ctrl_mux_out
 
 );
 
-assign all_control_out = (stall) ? '0: all_control_in;
+assign ctrl_mux_out = (stall) ? '0: all_control_in;
 
 
 endmodule
