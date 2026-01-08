78a303c5a16df7ef33660155ef9fc23a2e615c4d
omermumtaz05 <omermumtaz05@gmail.com>
2026-01-08 17:34:45 +0400

    Update all_modules.sv

--- all_modules.sv
+++ all_modules.sv
@@ -659,7 +659,6 @@ module mem_to_reg_mux(
     input logic [31:0] mem_read_data,
     input logic [31:0] ALU_result,
     input logic mem_to_reg,
-  	input logic stall,
 
     output logic [31:0] write_data
 
