3476f4bcb618a5d9146125af464c163dce426715
omermumtaz05 <omermumtaz05@gmail.com>
2026-01-08 18:11:56 +0400

    Update pipeline_regs.sv

--- pipeline_regs.sv
+++ pipeline_regs.sv
@@ -1,6 +1,3 @@
-//IF/ID register:
-
-
 import cpu_pkg::*;
 
 module IF_ID(
@@ -8,18 +5,20 @@ module IF_ID(
     input logic reset,
     input logic if_id_write,
     input if_id_data_t data_in,
+  
+    input if_id_flush,
 
     output if_id_data_t data_out
 );
 
     always_ff @ (posedge clock)
-      if(reset)
+      if(reset || if_id_flush)
             begin
                 data_out <= '0;
             end
       
         
-        else if(if_id_write)
+  	else if(if_id_write)
             begin
                 data_out <= data_in;
 
